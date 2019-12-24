1a7b4bde2fc3861c0287b2c2017454a5764b7ed0
rkant <rkant@ubuntu.ubuntu-domain>
2019-12-24 23:05:22 +0530

    FirstCommit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-	res.send({data:"Hello world!"});
+	res.send({data:"Hey I have changed something.....!"});
 })
 
 
