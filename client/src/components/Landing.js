3cd9e5c0ab3806135cb0a48773976b62d4d65d81
rkant <rkant@ubuntu.ubuntu-domain>
2020-01-06 03:57:29 +0530

    Added UI to display survey List

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,7 @@ class Landing extends React.Component{
     render(){
         return(
             <div style={{textAlign : 'center'}}>
-                <h2>|-------- EMAILY ---------|</h2>
+                <h2>|-------- Feedback collection App ---------|</h2>
                 <h4>Create Feedback form for your users.</h4>
                 <h4>There feedback will help you to improve your product quality.</h4>
                 <h2>{"<========>"}</h2>
